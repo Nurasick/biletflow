6782fc0daae53ba34ddb4faa2d61892ee98b8ae7
Shamil <yessengalishamil@gmail.com>
2026-09-12 13:40:03 +0500

    Initial login and auth setup

---
+++ mobile/src/app/login.tsx
@@ -0,0 +1,6 @@
+import {useState} from 'react';
+import { TextInput, TouchableOpacity, SafeAreaView, StatusBar } from 'react-native';
+import { useColorScheme } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
